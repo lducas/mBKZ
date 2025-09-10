ee0daa42f37889c2231b940fc15695ebb36078b7
Leo Ducas <ducas@cwi.nl>
2025-09-10 11:16:59 +0200

    fixing versionning issue

--- allexp_lwe.sh
+++ allexp_lwe.sh
@@ -1,5 +1,5 @@
-for seed in {0..14}; do
-    python3 exp_lwe.py n=120 verb=0 mBKZ=1 mKannan=1 c=15 ft=dd > ./data/lwe_240/mKan_mBKZ_seed${seed}.csv &
-    python3 exp_lwe.py n=120 verb=0 mBKZ=0 mKannan=1 c=15 ft=dd > ./data/lwe_240/mKan_BKZ_seed${seed}.csv &
-    python3 exp_lwe.py n=120 verb=0 mBKZ=0 mKannan=0 c=15 ft=dd > ./data/lwe_240/Kan_BKZ_seed${seed}.csv &
+for seed in {0..29}; do
+    python3 exp_lwe.py n=136 verb=0 mBKZ=1 mKannan=1 c=15 ft=dd > ./data/lwe_272/mKan_mBKZ_seed${seed}.csv &
+    python3 exp_lwe.py n=136 verb=0 mBKZ=0 mKannan=1 c=15 ft=dd > ./data/lwe_272/mKan_BKZ_seed${seed}.csv &
+    python3 exp_lwe.py n=136 verb=0 mBKZ=0 mKannan=0 c=15 ft=dd > ./data/lwe_272/Kan_BKZ_seed${seed}.csv &
 done
