8f14ebef85f30bff8368b0e341a8f1090cf54f90
Lynn Engelberts <64254210+lynnengelberts@users.noreply.github.com>
2025-09-18 15:14:47 +0200

    Update asympt_gain.py

--- asympt_gain.py
+++ asympt_gain.py
@@ -9,7 +9,10 @@ Ks = [Cyclotomic(c) for c in cs]
 
 def asympt_beta_eq(beta, K):
     d = K.deg
-    return beta + (log(abs(K.disc)) - d*log(d))*beta/(d*(log(beta/(2*pi))-2)) + d - 1
+    if abs(K.disc) == d**d:
+        return beta + d-1
+    else: 
+        return beta + (log(abs(K.disc)) - d*log(d))*beta/(d*(log(beta)))
 
 writer = csv.writer(open("data/asympt_gain.csv", 'w'))
 writer.writerow(['beta', 'beta_eq1' , 'beta_eq3' , 'beta_eq4' , 'beta_eq5' , 'beta_eq7' , 'beta_eq8' , 'beta_eq9' , 'beta_eq11', 'beta_eq13', 'beta_eq15', 'beta_eq16', 'beta_eq17', 'beta_eq32'])
@@ -17,4 +20,4 @@ writer.writerow(['beta', 'beta_eq1' , 'beta_eq3' , 'beta_eq4' , 'beta_eq5' , 'be
 print("beta, " + ", ".join([f"beta_eq{c}"+(" " if c<10 else "") for c in cs]))
 for beta in range(60, 1001, 10):
     print(f"{beta:4d}, " + ", ".join([f"{asympt_beta_eq(beta, K) - beta:9.3f}" for K in Ks]))
-    writer.writerow([asympt_beta_eq(beta, K) - beta for K in Ks])
+    writer.writerow([beta]+[asympt_beta_eq(beta, K) - beta for K in Ks])
